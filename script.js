a6306b44e791a85a1f89d54737a7e802d44f5e60
deniseduardo12n-arch <deniseduardo12n@gmail.com>
2026-03-10 12:05:11 -0700

    Weather app assignment

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
- document.getElementById("loadWeather").addEventListener("click", getWeather);
+ document.getElementById("loadTemp").addEventListener("click", getTemperature);
+document.getElementById("loadWind").addEventListener("click", getWind);
 
-function getWeather(){
+
+/* temperature endpoint */
+function getTemperature(){
 
 fetch("https://api.open-meteo.com/v1/forecast?latitude=34.03&longitude=-117.04&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto")
 
 .then(response => response.json())
 
 .then(data => {
-
-console.log(data);
 
 document.getElementById("temperature").textContent =
 "Temperature: " + data.current_weather.temperature + " °C";
@@ -19,9 +20,31 @@ document.getElementById("high").textContent =
 document.getElementById("low").textContent =
 "Low: " + data.daily.temperature_2m_min[0] + " °C";
 
+})
+
+.catch(error => {
+console.error("Error loading temperature:", error);
+});
+
+}
+
+
+/* wind endpoint */
+function getWind(){
+
+fetch("https://api.open-meteo.com/v1/forecast?latitude=34.03&longitude=-117.04&current_weather=true&timezone=auto")
+
+.then(response => response.json())
+
+.then(data => {
+
 document.getElementById("wind").textContent =
 "Wind Speed: " + data.current_weather.windspeed + " km/h";
 
+})
+
+.catch(error => {
+console.error("Error loading wind:", error);
 });
 
 }
